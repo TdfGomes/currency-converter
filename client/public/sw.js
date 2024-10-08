156a9dd4468da009a20481d76f3b9281823a583e
TdfGomes <t.dfgomes@icloud.com>
2024-10-08 01:19:00 +0100

    feat: add service worker to cache api calls and assets

---
+++ client/public/sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("fetch", (event) => {
+  const isImage = event.request.url.endsWith(".svg");
+  const isApi = /api/.test(event.request.url);
+  if (isImage || isApi) {
+    event.respondWith(
+      caches.open("app-cache").then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response) {
+            return response;
+          }
+
+          return fetch(event.request).then((networkResponse) => {
+            cache.put(event.request.url, networkResponse.clone());
+            return networkResponse;
+          });
+        });
+      }),
+    );
+  }
+});
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Root from "./Root";
 import reportWebVitals from "./reportWebVitals";
+import { register } from "serviceWorkerResgistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,6 +11,8 @@ root.render(
   </React.StrictMode>,
 );
 
+//register service worker
+register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
